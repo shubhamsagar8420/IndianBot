3d89756c749224d9312674054ff6b50e5a7cb80d
shubhamsagar8420 <54436698+shubhamsagar8420@users.noreply.github.com>
2020-05-26 12:33:50 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,7 +17,7 @@ async def amireallyalive(alive):
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "`Telethon version:` **6.0.9**\n`Python:` **3.7.4**\n"
                      "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
-                     "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
-                     "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
-                     "     [🇮🇳Deploy This IndianBot🇮🇳](https://github.com/indianbhaiya/IndianBot)") 
+                     "**Bot Creator:** [@Iam Shubham [/s/gang] // Lucy Gang //](https://t.me/honey66v1)\n"
+
+
 
